19d2e36f081355b493a5b95157cd17dba89e9c16
parideiveegan <44268353+parideiveegan@users.noreply.github.com>
2024-09-28 10:33:55 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -3,10 +3,10 @@ from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
 
-    def ReturnsEmptyStringForEmptyString(self):
+    def test_ReturnsEmptyStringForEmptyString(self):
         self.assertEqual(generate_soundex(""), '')
 
-    def RetrunsSodexcodeSingleCharacterInput(self):
+    def test_RetrunsSodexcodeSingleCharacterInput(self):
         self.assertEqual(generate_soundex("A"), "A000")
         self.assertEqual(generate_soundex("p"), "P000")
         self.assertEqual(generate_soundex("W"), "W000")
@@ -14,38 +14,38 @@ class TestSoundex(unittest.TestCase):
         self.assertEqual(generate_soundex("M"), "M000")
         self.assertEqual(generate_soundex("R"), "R000")
         
-    def RetrunsSoundexCodeRemovingInvalidCharInInput(self):
+    def test_RetrunsSoundexCodeRemovingInvalidCharInInput(self):
         self.assertEqual(generate_soundex("!pari"), "P600")
         self.assertEqual(generate_soundex("132pari"), "P600")
         self.assertEqual(generate_soundex("  pari"), "P600")
         self.assertEqual(generate_soundex(" - pari"), "P600")
     
     # To test the inputs with only invalid characters 
-    def ReturnsaZeroPaddedforAllCharAsInvalidInInput(self):
+    def test_ReturnsaZeroPaddedforAllCharAsInvalidInInput(self):
         self.assertEqual(generate_soundex("!=0234/"), "0000")
         self.assertEqual(generate_soundex("1"), "0000")
         self.assertEqual(generate_soundex("   "), "0000")
 
-    def RetursSondexCodeRemovingVowelsInInput(self):
+    def test_RetursSondexCodeRemovingVowelsInInput(self):
         self.assertEqual(generate_soundex("WWWWW"), "W000")
         self.assertEqual(generate_soundex("PAAWWW"), "P000")
         
-    def ReturnsSoundexCodeFirstCharReservedForVowelasFirstChar(self):
+    def test_ReturnsSoundexCodeFirstCharReservedForVowelasFirstChar(self):
         self.assertEqual(generate_soundex("RRRpari"), "R160")
         
-    def ReturnsSoundexCodeIgnoringReptedLettersInInput(self):
+    def test_ReturnsSoundexCodeIgnoringReptedLettersInInput(self):
         self.assertEqual(generate_soundex("Butter"), "B360")
         
-    def ReturnsSoundexCodeIgnoringCosecutivelettersSepratedByHWYInInput(self):
+    def test_ReturnsSoundexCodeIgnoringCosecutivelettersSepratedByHWYInInput(self):
         self.assertEqual(generate_soundex("Ashcraft"), "A261")
         
-    def ReturnsSoundexCodeReservingCosecutivelettersSepratedByVowelsInInput(self):
+    def test_ReturnsSoundexCodeReservingCosecutivelettersSepratedByVowelsInInput(self):
         self.assertEqual(generate_soundex("Tymczak"), "T522")
         
-    def ReturnsSoundexCodeLimitingCodeLengthToFourByPaddingZeroOrTruncating(self):
+    def test_ReturnsSoundexCodeLimitingCodeLengthToFourByPaddingZeroOrTruncating(self):
         self.assertEqual(generate_soundex("All"), "A400")
         
-    def ReturnsSoundexCodeKeppingTheFirstLetterSameAsInTheInput(self):
+    def test_ReturnsSoundexCodeKeppingTheFirstLetterSameAsInTheInput(self):
         self.assertEqual(generate_soundex("Pari"), "P600")
     
 if __name__ == '__main__':
